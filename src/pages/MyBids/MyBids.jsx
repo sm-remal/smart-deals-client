c154ceae9b63106fb473235521a0a1dfa41c6359
sm-remal <remal.mozzem@gmail.com>
2025-11-05 02:34:11 +0600

    update code

--- src/pages/MyBids/MyBids.jsx
+++ src/pages/MyBids/MyBids.jsx
@@ -14,12 +14,16 @@ const MyBids = () => {
   useEffect(() => {   
     // Only fetch if user email is available
     if (user?.email) {
-      fetch(`http://localhost:3000/my-bids?email=${user.email}`)
+      fetch(`http://localhost:3000/my-bids?email=${user.email}`, {
+        headers: {
+          authorization: `Bearer ${user.accessToken}`
+        }
+      })
         .then((res) => res.json())
         .then((data) => setBids(data)) // Save bids in local state
         .catch((err) => console.error("Error fetching bids:", err));
     }
-  }, [user?.email]);
+  }, [user]);
 
   // ================== Handle Delete Bid ==================
   const handleDeleteBids = (id) => {
